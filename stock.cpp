1f591963c9155cb2e09a4b1e477a0ac30190dd9a
olvr446 <olli@gloverdesign.co.uk>
2025-09-20 10:55:07 +0000

    commit

--- stock.cpp
+++ stock.cpp
@@ -2,6 +2,32 @@
 #include <string> 
 #include <vector>
 
+void main() {
+	int choice;
+	do {
+		std::cout << " //Welcome to Stock Sim!\\ \n";
+		std::cout << "1. View Stock Market";
+		std::cout << "2. Buy Stocks";
+		std::cout << "3. Sell Stocks";
+		std::cout << "4. Update Prices (Next Day)";
+		std::cout << "5. View Portfolio";
+		std::cout << "6. Load Portfolio";
+		std::cout << "7. Save & Exit";
+		std::cin >> choice;
+
+		// switch(choice){
+		// 	case 1: 
+		// 	case 2: 
+		// 	case 3: 
+		// 	case 4:
+		// 	case 5:
+		// 	case 6:
+		// 	case 7:
+		// }
+	}
+	while (choice != 7);
+};
+
 class Stock {
 	private:
 		std::string stockID;
@@ -48,6 +74,21 @@ class Port {
 			//
 		};
 
+		void depositCash(double amount) {
+			//
+		};
+
+		void withdrawCash(double amount) {
+			//
+		};
+
+		void saveFile() {
+
+		};
+
+		void loadFile() {
+
+		};
 
 
 
