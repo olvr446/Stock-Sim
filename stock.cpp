6cf25e8f8defbf873807df38e7b6c59a0c084b53
olvr446 <olli@gloverdesign.co.uk>
2025-09-20 10:12:49 +0000

    commit

--- stock.cpp
+++ stock.cpp
@@ -1,4 +1,43 @@
+#include <iostream>
+#include <string> 
+#include <vector>
+
 struct Stock {
+	std::string stockID;
 	std::string name;
 	double price;
+
+	double getPrice() {
+		return price;
+	};
+
+	void updatePrice() {
+		//
+	};
+};
+
+struct PortItem {
+	std::string name;
+	int quantity;
+};
+
+class Port {
+	private:
+		double balance;
+		std::vector <PortItem> holdings;
+
+	public:
+		void buyStock(std::string stockID, int quantity) {
+			//
+		};
+
+		void sellStock(std::string stockID, int quantity){
+			//
+		};
+
+		std::vector<double> getPortVal() {
+
+		};
+
+
 };
